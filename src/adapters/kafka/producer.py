3f8ba55cda7e29631629678ace22c053c18331ef
Аrinabitcoin <arina.sokolova2007@gmail.com>
2026-05-16 23:09:30 +0300

    fixes of messages

--- src/adapters/kafka/producer.py
+++ src/adapters/kafka/producer.py
@@ -4,3 +4,5 @@ from aiokafka import AIOKafkaProducer as AsyncKafkaProducer
 class KafkaProducer:
     def __init__(self, producer: AsyncKafkaProducer) -> None:
         self._producer = producer
+
+    def start(self): ...
--- src/adapters/kafka/topics.py
+++
@@ -1,9 +0,0 @@
-TOPICS = [
-    "team_service.team.created",
-    "team_service.team.submitted",
-    "team_service.team.updated",
-    "team_service.member.kicked",
-    "team_service.member.left",
-    "invitation_service.invitation.accepted",
-    "invitation_service.join_request.accepted",
-]
