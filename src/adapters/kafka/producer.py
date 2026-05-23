e5dfa9d7ec083cd2bb735dbb936089b23514a92a
Аrinabitcoin <arina.sokolova2007@gmail.com>
2026-05-23 20:33:38 +0300

    feature/#11 added pyright fixes

--- src/adapters/kafka/producer.py
+++ src/adapters/kafka/producer.py
@@ -4,3 +4,14 @@ from aiokafka import AIOKafkaProducer as AsyncKafkaProducer
 class KafkaProducer:
     def __init__(self, producer: AsyncKafkaProducer) -> None:
         self._producer = producer
+
+    async def start(self) -> None: ...
+
+    async def stop(self) -> None: ...
+
+    async def send(
+        self,
+        topic: str,
+        key: bytes,
+        value: bytes,
+    ) -> None: ...
